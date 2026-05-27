7ec17e4917ee9f9e810d6a4a9d67266074e26c29
michaelbfs <michael.leuenberger@bfs.admin.ch>
2026-05-27 15:01:18 +0000

    fin de journée

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
+# %%
 from dotenv import load_dotenv
+import os
+from openai import OpenAI
+from qdrant_client import QdrantClient
+import duckdb
+# %%
+
 load_dotenv()
 
-import os
+
 try:
     QDRANT_URL = os.environ["QDRANT_URL"]
     print("QDRANT_URL loaded successfully")
@@ -14,7 +21,7 @@ os.getcwd()
 
 
 # %%
-from openai import OpenAI
+
 
 client_llmlab = OpenAI(
     base_url=os.environ["LLMLAB_URL"],
@@ -26,6 +33,30 @@ models = client_llmlab.models.list()
 for model in models.data:
     print(f"ID: {model.id}")
 
+# %%
+client_qdrant = QdrantClient(
+    url=os.environ["QDRANT_URL"],
+    api_key=os.environ["QDRANT_API_KEY"],
+    port=os.environ["QDRANT_API_PORT"],
+    check_compatibility=False
+)
+
+collections = client_qdrant.get_collections()
+for collection in collections.collections:
+    print(collection.name)
 
 
+# %%
+
+con = duckdb.connect(database=":memory:")
+
+con.execute("INSTALL httpfs;")
+con.execute("LOAD httpfs;")
+
+path_nace = 'https://minio.lab.sspcloud.fr/projet-formation/diffusion/funathon/2026/project2/NACE_Rev2.1_Structure_Explanatory_Notes_EN.tsv'
+query_definition = f"SELECT * FROM read_csv('{path_nace}')"
+table = con.execute(query_definition).to_arrow_table()
+nace = table.to_pylist()
+
+nace[22]
 # %%
